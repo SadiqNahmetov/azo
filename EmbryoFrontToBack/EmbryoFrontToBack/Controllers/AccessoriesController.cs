36ae25bffe8dca89659f910f8a3bc05c7b4b614f
SadiqNahmetov <sadigfn@code.edu.az>
2022-10-16 17:02:58 +0400

    Azo

---
+++ EmbryoFrontToBack/EmbryoFrontToBack/Controllers/AccessoriesController.cs
@@ -0,0 +1,103 @@
+﻿using EmbryoFrontToBack.Data;
+using EmbryoFrontToBack.Models;
+using EmbryoFrontToBack.ViewModels;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmbryoFrontToBack.Controllers
+{
+    public class AccessoriesController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public AccessoriesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+
+            //HttpContext.Session.SetString("name", "Sadiq");
+            //Response.Cookies.Append("surname", "Nahmetov", new CookieOptions { MaxAge = TimeSpan.FromDays(5)});
+              
+            ViewBag.count = await _context.Accessories.Where(m=>!m.IsDeleted).CountAsync();
+
+            IEnumerable<Accessories> accessories = await _context.Accessories.Where(m=>!m.IsDeleted).Take(3).OrderBy(m=>m.Id).ToListAsync();
+
+            AccessoriesVM accessoriesVM = new AccessoriesVM
+            {
+               Accessories = accessories
+            };
+            return View(accessoriesVM);
+        }
+
+        public async Task<IActionResult> LoadMore(int skip)
+        {
+            IEnumerable<Accessories> accessories = await _context.Accessories.Where(m=>!m.IsDeleted).Skip(skip).Take(3).ToListAsync();
+            return PartialView("_ProductPartial", accessories);
+        }
+
+        //public IActionResult Test()
+        //{
+        //    var sessionData = HttpContext.Session.GetString("name");
+        //    var cookieData = Request.Cookies["surname"];
+        //    return Json(sessionData + "-" + cookieData);
+        //}
+
+
+        [HttpPost]
+        public async Task<IActionResult> AddBasket(int? id)
+        {
+            if (id is null)
+            {
+                return BadRequest();
+            }
+            //var dbAccesories = await _context.Accessories.FirstOrDefaultAsync(m =>m.Id == id);
+            
+            var dbAccesories = await _context.Accessories.FindAsync(id);
+         
+            if (dbAccesories == null)
+            {
+                return NotFound();
+            }
+
+            List<BasketVM> basket;
+
+            if (Request.Cookies["basket"]!= null)
+            {
+                basket = JsonConvert.DeserializeObject<List<BasketVM>>(Request.Cookies["basket"]);
+            }
+            else
+            {
+                basket = new List<BasketVM>();
+            }
+
+            BasketVM existProduct = basket.FirstOrDefault(m => m.Id == dbAccesories.Id);
+
+            if (existProduct == null)
+            {
+                basket.Add(new BasketVM
+                {
+                    Id = dbAccesories.Id,
+                    Count = 1
+                }); ;
+            }
+            else
+            {
+                existProduct.Count++;
+            }
+
+          
+
+            Response.Cookies.Append("basket", JsonConvert.SerializeObject(basket));
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ EmbryoFrontToBack/EmbryoFrontToBack/Startup.cs
@@ -0,0 +1,58 @@
+using EmbryoFrontToBack.Data;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+
+namespace EmbryoFrontToBack
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSession(option =>
+            {
+                option.IdleTimeout = TimeSpan.FromSeconds(50);
+            });
+            services.AddControllersWithViews();
+            services.AddDbContext<AppDbContext>(options =>
+            {
+                options.UseSqlServer(Configuration.GetConnectionString("DefaultConection"));
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+           
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseSession();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
